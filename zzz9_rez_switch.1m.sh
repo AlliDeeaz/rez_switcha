951a4c36250e9dbb8c2224efd91a6f4ddc21fa94
ALLiDEEAZ <119712851+AlliDeeaz@users.noreply.github.com>
2025-05-04 21:40:55 +0200

    Add files via upload
    ## Features
    
    - Quick change between 1280×720, 1920×1080, and 3840×2160
    - Color-coded, high-contrast menu items
    - minimalistic & simple

---
+++ zzz9_rez_switch.1m.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# <xbar.title>Z-RezSwitch</xbar.title>
+# <xbar.version>v1.3</xbar.version>
+# <xbar.author>Swift Classroom</xbar.author>
+# <xbar.desc>Switch display resolution with readable color/font settings</xbar.desc>
+# <xbar.dependencies>displayplacer</xbar.dependencies>
+
+echo "🖥"
+echo "---"
+echo "1280x720 | bash='/opt/homebrew/bin/displayplacer' param1='id:B864F3FA-1829-40B8-BA2A-12A78E4DD5BA res:1280x720 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0' terminal=false refresh=true color=green font='Menlo 14'"
+echo "1920x1080 | bash='/opt/homebrew/bin/displayplacer' param1='id:B864F3FA-1829-40B8-BA2A-12A78E4DD5BA res:1920x1080 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0' terminal=false refresh=true color=blue font='Menlo 14'"
+echo "3840x2160 | bash='/opt/homebrew/bin/displayplacer' param1='id:B864F3FA-1829-40B8-BA2A-12A78E4DD5BA res:3840x2160 hz:60 color_depth:8 origin:(0,0) degree:0' terminal=false refresh=true color=red font='Menlo Bold 16'"
